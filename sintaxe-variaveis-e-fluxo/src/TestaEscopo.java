7d5d7d1125e9c6d21c4ba7ef14514d034b63a115
igorsrs2019 <ighor_souza@hotmail.com>
2022-05-19 16:01:46 -0300

    Criando um programa sobre imposto de renda

--- sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -1,13 +1,23 @@
 
-public class TestaCondicional2 {
+public class TestaEscopo {
 
-	
 	public static void main(String[] args) {
 
 		System.out.println("Testando Condicionais");
-		int idade = 18;
+		
+		int idade = 20;
 		int quantidadePessoas = 3;
-		boolean acompanhado = quantidadePessoas >=2;
+		
+		//boolean acompanhado = quantidadePessoas >=2;
+		 
+		 boolean acompanhado;
+
+		  
+		if (quantidadePessoas  >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
 		
 		System.out.println("Valor de acompanhando = " + acompanhado);
 		
@@ -21,6 +31,7 @@ public class TestaCondicional2 {
 		}
 			
 		}
-	}
 	
 	
+	
+}
